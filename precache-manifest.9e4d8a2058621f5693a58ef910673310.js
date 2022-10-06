7056e864786af3fccdafa9e3feacee000dc6d4f6
Arthur Wang <arthur.wang@yale.edu>
2022-10-06 18:53:28 -0400

    Updates

---
+++ precache-manifest.9e4d8a2058621f5693a58ef910673310.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f8ced686c3ea9b440a4",
+    "url": "/special-issues/static/css/main.a2ebc901.chunk.css"
+  },
+  {
+    "revision": "6f8ced686c3ea9b440a4",
+    "url": "/special-issues/static/js/main.6f8ced68.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/special-issues/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/special-issues/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "154a52e6a35bee1a77f7",
+    "url": "/special-issues/static/js/runtime~main.154a52e6.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/special-issues/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "98673374bc8ac70e1320820e6c804466",
+    "url": "/special-issues/index.html"
+  }
+];
